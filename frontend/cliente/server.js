bcdcbca9ab02d2af93d3ac64e1658ccedd7703b5
sergiorodriguez25 <32263950+sergiorodriguez25@users.noreply.github.com>
2019-10-19 11:57:41 +0200

    'aJDSJAD'

--- frontend/cliente/server.js
+++ frontend/cliente/server.js
@@ -6,11 +6,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/SGC_E1/frontend/cliente'));
+app.use(express.static(__dirname + '/dist/cliente'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/SGC_E1/frontend/cliente/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/cliente/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
