564723d34a02c98aa409ad6fa71d254f9756f1d7
Felipe Lozano Romero <felipe.lozano1@alu.uclm.es>
2019-10-28 23:14:05 +0100

    Todo al raiz

---
+++ src/main/java/com/example/backend/models/services/UsuarioServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.backend.models.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.backend.models.dao.IUsuarioDAO;
+import com.example.backend.models.entity.Usuario;
+
+@Service
+public class UsuarioServiceImpl implements IUsuarioService{
+
+	@Autowired
+	private IUsuarioDAO usuarioDao;
+	
+	@Override
+	public List<Usuario> findAll() {
+		return usuarioDao.findAll();
+	}
+
+	@Autowired(required = false)
+	public Usuario findUserByDni(String dni) {
+		return usuarioDao.findByDni(dni);
+	}
+	
+	
+
+	
+}
