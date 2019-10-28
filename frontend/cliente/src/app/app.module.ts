7acfa3a2dcc3102bf4b484edbd55a14dde12847f
Felipe Lozano Romero <felipe.lozano1@alu.uclm.es>
2019-10-29 00:54:56 +0100

    SGC-1201

--- frontend/cliente/src/app/app.module.ts
+++ frontend/cliente/src/app/app.module.ts
@@ -9,19 +9,29 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { CitasComponent } from './components/citas/citas.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+// Services
+import { LoginService } from './components/login/login.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     CitasComponent,
-    RegistroComponent
+    RegistroComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule, 
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
