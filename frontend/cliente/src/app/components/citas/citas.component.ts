5d32fc08b10652548a0b16f7b4e8dd3f6a152033
sergiorodriguez25 <32263950+sergiorodriguez25@users.noreply.github.com>
2019-10-31 13:35:33 +0100

    add SGC 07 02

--- frontend/cliente/src/app/components/citas/citas.component.ts
+++ frontend/cliente/src/app/components/citas/citas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ChildActivationStart } from '@angular/router';
 
 @Component({
   selector: 'app-citas',
@@ -8,11 +8,11 @@ import { Router } from '@angular/router';
 })
 export class CitasComponent implements OnInit {
 
-  citas = [{fecha: '13/10/2019', consulta: '37', medico: 'Antonio Ramos Romero', especialidad: 'Traumatología'},
-           {fecha: '15/10/2019', consulta: '45', medico: 'Luis Jimenez Roldán', especialidad: 'Neurología'},
-           {fecha: '17/10/2019', consulta: '65', medico: 'María Torras Catalán', especialidad: 'Urgencias'},
-           {fecha: '23/11/2019', consulta: '23', medico: 'Manuel Puente Garcia', especialidad: 'Oncología'},
-           {fecha: '26/11/2019', consulta: '67', medico: 'Paula Bermejo Delgado', especialidad: 'Nefrología'}
+  citas = [{id:'1', fecha: '13/10/2019', consulta: '37', medico: 'Antonio Ramos Romero', especialidad: 'Traumatología'},
+           {id:'2', fecha: '15/10/2019', consulta: '45', medico: 'Luis Jimenez Roldán', especialidad: 'Neurología'},
+           {id:'3', fecha: '17/10/2019', consulta: '65', medico: 'María Torras Catalán', especialidad: 'Urgencias'},
+           {id:'4', fecha: '23/11/2019', consulta: '23', medico: 'Manuel Puente Garcia', especialidad: 'Oncología'},
+           {id:'5', fecha: '26/11/2019', consulta: '67', medico: 'Paula Bermejo Delgado', especialidad: 'Nefrología'}
           ];
 
   constructor(private router:Router) { }
@@ -25,5 +25,9 @@ export class CitasComponent implements OnInit {
   modificardatoscontacto(){
     this.router.navigate(['/cambiodatoscontacto'])
   }
-
+ 
+  eliminarcita(cita){
+    var index=this.citas.indexOf(cita);
+    this.citas.splice(index, 1);
+  }
 }
