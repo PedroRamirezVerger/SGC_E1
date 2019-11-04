a245d8c2c3f6b3cbf5b50e9a1cdac999fb79905a
sergiorodriguez25 <32263950+sergiorodriguez25@users.noreply.github.com>
2019-11-04 10:35:01 +0100

    metodos validar

--- frontend/cliente/src/app/components/registro/registro.component.ts
+++ frontend/cliente/src/app/components/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Usuario } from 'src/app/entity/Usuario';
 import { RegistroService } from './registro.service';
+import { isNumber } from 'util';
 
 @Component({
   selector: 'app-registro',
@@ -20,6 +21,34 @@ export class RegistroComponent implements OnInit {
   ngOnInit() {
   }
 
+  validarletradni(letra:string){
+    var abecedario="abcdefghyjklmnñopqrstuvwxyz";
+    letra = letra.toLowerCase();
+   for(var i=0; i<letra.length; i++){
+      if (abecedario.indexOf(letra.charAt(i),0)!=-1){
+         return true;
+      }
+   }
+   return false;
+  }
+  validardni(dni:string) {
+    var numero=dni.substring(0,dni.length-1);
+    var letra=dni.charAt(dni.length-1);
+    
+    if(dni.length != 9 || !isNumber(numero) || this.validarletradni(letra)){
+      this.msg = 'Introduce el dni de manera correcta.'
+      return false;
+    }
+    return true;
+  }
+  validartelefono(telefono:string){
+    if( telefono.length!= 9 || !isNumber(telefono)){
+      this.msg = 'Introduce el telefono de manera correcta.'
+      return false;
+    }
+    return true;
+  }
+
   singUp(nombre: string, apellidos: string, dni:string, password: string, telefono: string, email: string, sexo: string, fechaNacimiento: Date, tipo: string){
     console.log(nombre);
     this.usuario.nombre = nombre;
@@ -33,7 +62,7 @@ export class RegistroComponent implements OnInit {
     this.usuario.tipo = "usuario";
     console.log(this.usuario);
 
-    if(nombre.length == 0|| apellidos.length == 0 || dni.length == 0 || password.length == 0 || telefono.length == 0 
+    if(nombre.length == 0|| apellidos.length == 0 || this.validardni(dni) || password.length == 0 || this.validartelefono(telefono) 
       || email.length == 0 || sexo.length == 0 || fechaNacimiento == null || tipo.length == 0) {
       this.msg = 'Tienes que rellenar todos los campos.'
    }
