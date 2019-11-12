d8eb6c2b2f29394080effd0968c42913ccb99a55
Felipe Lozano Romero <felipe.lozano1@alu.uclm.es>
2019-11-12 01:07:00 +0100

    Cambio crosorigin

--- src/main/java/com/example/backend/controllers/CitaRestController.java
+++ src/main/java/com/example/backend/controllers/CitaRestController.java
@@ -23,8 +23,8 @@ import com.example.backend.models.services.IUsuarioService;
 import lombok.Delegate;
 
 
-// @CrossOrigin(value = "https://sgcequipo1.herokuapp.com") 
-@CrossOrigin(value = "http://localhost:4200") // PARA DESARROLLO
+@CrossOrigin(value = "https://sgcequipo1.herokuapp.com") 
+// @CrossOrigin(value = "http://localhost:4200") // PARA DESARROLLO
 @RestController
 @RequestMapping("api")
 public class CitaRestController {
--- src/main/java/com/example/backend/controllers/UsuarioRestController.java
+++ src/main/java/com/example/backend/controllers/UsuarioRestController.java
@@ -31,8 +31,8 @@ import com.example.backend.models.entity.Usuario;
 import com.example.backend.models.respuesta.RespuestaLogin;
 import com.example.backend.models.services.IUsuarioService;
 
-// @CrossOrigin(value = "https://sgcequipo1.herokuapp.com") 
-@CrossOrigin(value = "http://localhost:4200") // PARA DESARROLLO
+@CrossOrigin(value = "https://sgcequipo1.herokuapp.com") 
+// @CrossOrigin(value = "http://localhost:4200") // PARA DESARROLLO
 @RestController
 @RequestMapping("api")
 public class UsuarioRestController {
