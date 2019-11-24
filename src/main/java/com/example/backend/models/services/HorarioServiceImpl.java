5a6c14228d0bb5904b92d6c1cedf9af984465262
PedroRamirezVerger <32263959+PedroRamirezVerger@users.noreply.github.com>
2019-11-24 21:21:23 +0100

    save y delete horario

--- src/main/java/com/example/backend/models/services/HorarioServiceImpl.java
+++ src/main/java/com/example/backend/models/services/HorarioServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.backend.models.services;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.example.backend.models.dao.IHorarioDAO;
 import com.example.backend.models.entity.Horario;
 
@@ -16,6 +18,22 @@ public class HorarioServiceImpl implements IHorarioService{
 	@Override
 	public Horario findHorarioByDnimedicoAndDiaAndMesAndAno(String dniMedico, int dia, int mes, int ano) {
 		return horarioDao.findByDniMedicoAndDiaAndMesAndAno(dniMedico, dia, mes, ano);
+	}
+
+	@Override
+	public List<Horario> findHorariosByDniMedico(String dniMedico) {
+		return horarioDao.findByDniMedico(dniMedico);
+	}
+
+	@Override
+	public void saveHorario(Horario horario) {
+		horarioDao.save(horario);
+		
+	}
+
+	@Override
+	public void deleteHorario(@Valid String id) {
+		horarioDao.deleteById(id);
 	}
 
 }
--- src/main/java/com/example/backend/models/services/IHorarioService.java
+++ src/main/java/com/example/backend/models/services/IHorarioService.java
@@ -2,10 +2,15 @@ package com.example.backend.models.services;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.example.backend.models.entity.Horario;
 
 public interface IHorarioService {
 	
 	public List<Horario> findAll();
 	public Horario findHorarioByDnimedicoAndDiaAndMesAndAno(String dniMedico, int dia, int mes, int ano);
+	public List<Horario> findHorariosByDniMedico(String dniMedico);
+	public void saveHorario(Horario horario);
+	public void deleteHorario(@Valid String id);
 }
