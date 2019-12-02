784e07204a497898542a4ea4771ec91135424145
Antonio Pulido Hernández <Antonio.Pulido@alu.uclm.es>
2019-12-02 11:57:06 +0100

    Aplicacion movil

--- src/main/java/com/example/backend/controllers/CitaRestController.java
+++ src/main/java/com/example/backend/controllers/CitaRestController.java
@@ -35,7 +35,7 @@ import com.example.backend.models.services.IUsuarioService;
 import com.example.backend.models.utiles.Encriptador;
 
 //@CrossOrigin(value = "https://sgcequipo1.herokuapp.com") 
-@CrossOrigin(value = "http://localhost:4200") // PARA DESARROLLO
+@CrossOrigin(value = "*") // PARA DESARROLLO
 @RestController
 @RequestMapping("api")
 public class CitaRestController {	
